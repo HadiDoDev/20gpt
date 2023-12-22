c36b2c951eb52993ef1ae10aa395f8639431b46d
hadido <dodangeh.hadi@gmail.com>
2023-12-22 21:47:43 +0330

    Fix vision handle

--- bot/eboo_utils.py
+++ bot/eboo_utils.py
@@ -28,5 +28,5 @@ def convert(filetoken):
     data['output'] = 'txtraw'
 
     res = requests.post(url, data=data)
-    print("Convert Res:", res.json(), flush=True)
+    print("Convert Res:", res.content, flush=True)
     return res.json()
