2a49e83cf9768f0e7bf0280d08a845319e540d0c
Hadi Do <dodangeh.hadi@gmail.com>
2023-12-30 19:46:53 +0330

    Change regex

--- bot/bot.py
+++ bot/bot.py
@@ -971,7 +971,7 @@ def run_bot() -> None:
     application.add_handler(MessageHandler(filters.PHOTO & user_filter, vision_message_handle))
 
     application.add_handler(CommandHandler("mode", show_chat_modes_handle, filters=user_filter))
-    application.add_handler(CallbackQueryHandler(show_chat_modes_callback_handle, pattern='^show_chat_modes\|.*$'))
+    application.add_handler(CallbackQueryHandler(show_chat_modes_callback_handle, pattern="^show_chat_modes"))
     application.add_handler(CallbackQueryHandler(set_chat_mode_handle, pattern="^set_chat_mode"))
 
     application.add_handler(CommandHandler("settings", settings_handle, filters=user_filter))
